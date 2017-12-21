dfcbd4174826e8f6e0f0c0a48ec0b92941e26221
karthyk-PC\karthyk <karthickted@gmail.com>
2017-12-21 06:47:01 -0600

    Second Commit with Post http

--- AngularJs/JS/myController.js
+++ AngularJs/JS/myController.js
@@ -15,12 +15,28 @@ myController.config(['$routeProvider',function($routeProvider, $location){
 }]);
 
 myController.controller("myController",["$scope","$location","$http",function($scope,$location,$http){
-	$scope.data="Block1";
+	$scope.data="CRUD Operation";
 	$scope.go = function ( path ) {
   $location.path( path );
 };
 $http.get('data/data.json').success(function(data){
 	$scope.value=data;
 });
-console.log(angular.toJson($scope.value));
+console.log(angular.toJson($scope.student));
+
+$scope.studentName = function () {
+
+							 var onSuccess = function (data, status, headers, config) {
+									 alert('Student saved successfully.');
+							 };
+
+							 var onError = function (data, status, headers, config) {
+									 alert('Error occured.');
+							 }
+
+							 $http.post('/data/student', { student:$scope.student })
+									 .success(onSuccess)
+									 .error(onError);
+
+					 };
 }]);
